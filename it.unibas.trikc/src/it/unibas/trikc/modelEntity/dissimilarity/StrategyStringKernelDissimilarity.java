db92274224eee872c14b0132c3d26adb50f2ccfa
angelo mecca <angelo mecca>
2016-07-11 15:52:03 +0200

    fixed Dissimilarity 2.0 - Team Dissimilarity

--- it.unibas.trikc/src/it/unibas/trikc/modelEntity/dissimilarity/StrategyStringKernelDissimilarity.java
+++ it.unibas.trikc/src/it/unibas/trikc/modelEntity/dissimilarity/StrategyStringKernelDissimilarity.java
@@ -1,5 +1,6 @@
 package it.unibas.trikc.modelEntity.dissimilarity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cc.mallet.types.StringKernel;
@@ -14,27 +15,43 @@ public class StrategyStringKernelDissimilarity implements IStrategyDissimilarity
 	@Override
 	public DissimilarityMatrix computeDissimilarity(TestSuite testSuite) {
 		testCases = testSuite.getTestCases();
+		testCases = checkEmptyTestCase(testCases);
 		DissimilarityMatrix dissimilarityMatrix = new DissimilarityMatrix(testCases.size());
 		dissimilarityMatrix.setHeaders(testCases);
 		double dissimilarity = 0;
 		for (int i = 0; i < testCases.size(); i++) {
 			for (int j = i; j < testCases.size() ; j++) {
-				dissimilarity = compareTestCases(testSuite.getTestCaseAt(i),testSuite.getTestCaseAt(j));
+				dissimilarity = compareTestCases(testCases.get(i),testCases.get(j));
 				dissimilarityMatrix.setValueAt(i, j,dissimilarity);
 				dissimilarityMatrix.setValueAt(j, i,dissimilarity);
-			//	System.out.println("In compute dissimilarity " + dissimilarity);
-			  //System.out.println("In compute dissimilarity valore a " + i + j +" "+  dissimilarityMatrix.getValueAt(i, j));
 				
 			}
 		}
 		return dissimilarityMatrix;
 	}
 	
+	private List<TestCase> checkEmptyTestCase(List<TestCase> testCases2) {
+		List <TestCase> backingTestCase = new ArrayList<>();
+		for (TestCase testCase : testCases2) {
+			if(testCase.getCoveredLines().size() != 0){
+				backingTestCase.add(testCase);
+			}
+		}
+		return backingTestCase;
+	}
+
 	private double compareTestCases(TestCase t1, TestCase t2){
 		StringKernel stringKernel = new StringKernel();
+		TestCase a = new TestCase(); 
+		if (t1.getCoveredLines().size() < t2.getCoveredLines().size()) {
+			a = t2;  
+			t2 = t1; 
+			t1 = a; 
+		}
 		double [] dissimilarity = new double [t1.getCoveredLines().size()] ;
 		double dissimilarityTrue = 1;
 		 for (int i = 0; i < t1.getCoveredLines().size(); i++) {
+			dissimilarityTrue = 1.0;
 			for (int j = 0; j < t2.getCoveredLines().size(); j++) {
 				double dissimilarityBacking = 1;
 				dissimilarityBacking = (1-stringKernel.K(t1.getCoveredLineAt(i).getId(), t2.getCoveredLineAt(j).getId()));
